0e4149ade80e9c2b5f3523cefc39dc47b08fa468
Shresth <shresthgkohli@gmail.com>
2026-06-19 19:51:00 +0530

    yo

--- Express/services/authService.js
+++ Express/services/authService.js
@@ -104,9 +104,7 @@ async function login(username, password) {
     const sessionId =
         await sessionService.createSession(user.id);
 
-    return {
-        sessionId: sessionId
-    };
+    return sessionId;
 
 }
 
